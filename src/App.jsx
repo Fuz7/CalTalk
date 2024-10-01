a647590556d4c9f5e811b5001b3a2b0a842f43d4
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-10-01 15:07:47 +0800

    Add languages dropdown collapse animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-
+import LanguageDropdown from "./components/LanguageDropdown";
 
 function App() {
   
   return (
-   <div className="min-h-lvh min-w-full bg-[#FFEBCD] flex
-   justify-center">
-    <section className="max-w-[1440px] min-w-[1440px] min-h-lvh 
-     relative flex flex-col">
-      <h1 className="max-w-[440px] min-w-[440px] min-h-[100px] 
+    <div
+      className="min-h-lvh min-w-full bg-[#FFEBCD] flex
+   justify-center"
+    >
+      <section
+        className="max-w-[1440px] min-w-[1440px] min-h-lvh 
+     relative flex flex-col"
+      >
+        <h1
+          className="max-w-[440px] min-w-[440px] min-h-[100px] 
       flex text-white mt-[35px]
       font-Belanosima items-center justify-center bg-[#664229]
       text-[64px] rounded-[20px] self-center
-      titleShadow">Caltalk</h1>
-      <div className=""></div>
-     </section>
-   </div>
-  )
+      titleShadow"
+        >
+          Caltalk
+        </h1>
+        <div className="flex justify-between">
+          <button className="min-w-[184px] min-h-[70px]
+          flex items-center justify-center text-white 
+          font-Roboto-Black text-[24px] bg-[#664229] 
+          rounded-[8px] 
+          shadow-[0_4px_4px_0px_rgba(0,0,0,0.34)] ">Instructions</button>
+          <LanguageDropdown />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/LanguageDropdown.jsx
@@ -0,0 +1,61 @@
+import dropdownArrow from '@assets/dropdownArrow.svg'
+import { useLayoutEffect, useState } from 'react'
+
+export default function LanguageDropdown(){
+  const [isCollapsed,setCollapsed] = useState(false)
+  const languages = ['English','Tagalog','Bisaya','Japanese']
+  const [barHeight,setBarheight] = useState(66)
+  useLayoutEffect(()=>{
+    if(isCollapsed){
+      setBarheight(66)
+    }else if(isCollapsed !== true){
+      setBarheight(66 + (languages.length * 60))
+    }
+  },[isCollapsed,languages.length])
+  return(
+    <div className='relative min-w-[184px] min-h-[66px] bg-transparent'>
+
+    <div className={`min-w-[184px] min-h-[66px]  rounded-[5px]
+    bg-white border border-solid border-[#664229]
+    absolute top-0 left-0 transition-[height]
+    flex flex-col  cursor-pointer overflow-clip 
+    select-none ease-out duration-300
+    `} 
+    style={{height:`${barHeight}px`}}
+    >
+    {languages.map((language,index) =>{
+        if(index === 0){
+          return(
+            <>
+              <div className={`flex justify-between items-center
+              min-h-[66px] pl-[18px] pr-[15px]  `}
+              onClick={()=> setCollapsed(!isCollapsed)}>
+
+              <p className="font-Righteous text-[24px]">{language}</p>
+              <img src={dropdownArrow} alt="" />
+              </div>
+              <div className='min-w-[184px] min-h-[60px] flex  items-center
+              pl-[18px] pr-[15px] border-black border-solid 
+              border-t-[1px]  hover:bg-[#f5f5f5]'>
+              <p className="font-Righteous text-[24px]">{language}</p>
+      
+              </div>
+            </>
+          )
+        }else{
+          return(
+            <>
+              <div className='min-w-[184px] min-h-[60px] flex  items-center
+              pl-[18px] pr-[15px] border-black border-solid 
+              border-t-[1px] hover:bg-[#f5f5f5]'>
+              <p className="font-Righteous text-[24px]">{language}</p>
+      
+              </div>
+            </>
+          )
+        }
+      })}
+    </div>
+    </div>
+  )
+}

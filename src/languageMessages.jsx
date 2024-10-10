6277edb705ad20ca058d40b3e725f27ba5d4ec15
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2024-10-10 22:37:13 +0800

    Fix some language messages spelling

--- src/languageMessages.jsx
+++ src/languageMessages.jsx
@@ -1,43 +1,44 @@
-export default function languageMessages(){
+export default function languageMessages() {
   const languageMessages = {
-    start:{
-      English:'Start Recording, and it will recognize your speech!',
-      Tagalog:'Simulan ang Pagre-record, at makikilala nito ang iyong pananalita',
-      Bisaya:'Sugdi ang Pagrekord, ug kini makaila sa imong sinultihan!',
-      Japanese:'Rokuon o kaishi suru to, anata no onsei ga ninshiki sa remasu.'
+    start: {
+      English: 'Start Recording, and it will recognize your speech!',
+      Tagalog:
+        'Simulan ang Pagre-record, at makikilala nito ang iyong pananalita',
+      Bisaya: 'Sugdi ang Pagrekord, ug kini makaila sa imong sinultihan!',
+      Japanese:
+        'Rokuon o kaishi suru to, anata no onsei ga ninshiki sa remasu.',
     },
-    emptyOutput:{
-      English:'Say something',
-      Tagalog:'Magsalita ka',
-      Bisaya:'Sultig usa ka butang',
-      Japanese:'Nani ka itte'
+    emptyOutput: {
+      English: 'Say something',
+      Tagalog: 'Magsalita ka',
+      Bisaya: 'Sultig usa ka butang',
+      Japanese: 'Nani ka itte',
     },
-    output:{
-      English:'Output: ',
-      Tagalog:'Resulta: ',
-      Bisaya:'Resulata: ',
-      Japanese:'Kekka: ',
+    output: {
+      English: 'Output: ',
+      Tagalog: 'Resulta: ',
+      Bisaya: 'Resulta: ',
+      Japanese: 'Kekka: ',
     },
-    incorrect:{
-      English:'False',
-      Tagalog:'Di totoo',
-      Bisaya:'Pataka raman ka dol',
-      Japanese:'Tadashikunai'
+    incorrect: {
+      English: 'False',
+      Tagalog: 'Di totoo',
+      Bisaya: 'Pataka raman ka dol',
+      Japanese: 'Tadashikunai',
     },
-    correct:{
-      English:'True',
-      Tagalog:'Tama',
-      Bisaya:'Tinuood Jud Na',
-      Japanese:'Tadashi',
+    correct: {
+      English: 'True',
+      Tagalog: 'Tama',
+      Bisaya: 'Tinuood Jud Na',
+      Japanese: 'Tadashi',
     },
-    wrongInput:{
-      English:'Invalid input, please try again',
-      Tagalog:'Di-wastong input, pakisubukang muli',
-      Bisaya:'Mali imong pagsulat, usabi',
-      Japanese:'Nyūryoku ga mukōdesu. Mōichido o tameshi kudasai.'
-    }
+    wrongInput: {
+      English: 'Invalid input, please try again',
+      Tagalog: 'Di-wastong input, pakisubukang muli',
+      Bisaya: 'Mali imong pagsulat, usabi',
+      Japanese: 'Nyūryoku ga mukōdesu. Mōichido o tameshi kudasai.',
+    },
+  };
 
-  }
-
-  return languageMessages
+  return languageMessages;
 }
